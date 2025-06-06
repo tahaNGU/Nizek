6e274eea0c779cd758b73d2c3d92d2b1e77761c2
tahangudev-group <tahangudev@gmail.com>
2025-06-06 19:06:15 +0330

    init project

--- start.sh
+++ start.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-# تنظیم دسترسی‌ها
 if [ -d /var/www/html/storage ] && [ -d /var/www/html/bootstrap/cache ]; then
     chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
     chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
 fi
 
-# اجرای queue worker در پس‌زمینه
 echo "Starting Laravel queue worker..."
 php artisan queue:work --tries=3 &
 
-# اجرای php-fpm در جلوی برنامه
 echo "Starting php-fpm..."
 php-fpm
